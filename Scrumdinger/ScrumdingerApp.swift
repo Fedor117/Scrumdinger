dc2e53932f8672dda779aec848948bfa2d976ff5
Fedor Valiavko <fedor.valiavko@gmail.com>
2021-08-13 18:30:38 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Fedor Valiavko on 13/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
