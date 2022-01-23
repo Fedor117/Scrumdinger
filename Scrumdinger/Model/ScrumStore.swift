10dfb02ed3b6c24b686310895ba3151400b69e50
Fedor Valiavko <fedor.valiavko@gmail.com>
2022-01-23 01:56:31 +0000

    Added loading/saving scrums info to file

---
+++ Scrumdinger/Model/ScrumStore.swift
@@ -0,0 +1,56 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Fedor Valiavko on 23/01/2022.
+//
+
+import SwiftUI
+
+final class ScrumStore: ObservableObject {
+    @Published var scrums = [DailyScrum]()
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("scrums.data")
+    }
+    
+    static func load(completion: @escaping (Result<[DailyScrum], Error>) -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let fileURL = try fileURL()
+                guard let file = try? FileHandle(forReadingFrom: fileURL) else {
+                    DispatchQueue.main.async {
+                        completion(.success([]))
+                    }
+                    return
+                }
+                let scrums = try JSONDecoder().decode([DailyScrum].self, from: file.availableData)
+                DispatchQueue.main.async {
+                    completion(.success(scrums))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+    
+    static func save(scrums: [DailyScrum], completion: @escaping (Result<Int, Error>) -> Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let data = try JSONEncoder().encode(scrums)
+                let outputFile = try fileURL()
+                try data.write(to: outputFile)
+                DispatchQueue.main.async {
+                    completion(.success(scrums.count))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+}
